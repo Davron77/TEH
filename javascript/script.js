5f9f7234c58079a86ee82c0243e39760895dbd58
Davron77 <davronbekq467@gmail.com>
2022-04-19 15:44:22 +0500

    Navbar some big

--- javascript/script.js
+++ javascript/script.js
@@ -2,12 +2,16 @@ const navbarCollapse = document.querySelector(".navbar-collapse");
 const faBars = document.querySelector(".fa-bars");
 const faXmark = document.querySelector(".fa-xmark");
 const HeaderFooter = document.querySelector("#HeaderFooter");
+const navbarIconLogin = document.querySelector(".navbar-icon__login");
+const navbarDropdown = document.querySelector(".navbar-dropdown");
 
 const navbarToggler = document
   .getElementById("navbar-toggler")
   .addEventListener("click", function () {
     navbarCollapse.classList.toggle("open");
     HeaderFooter.classList.toggle("open");
+    navbarDropdown.classList.toggle("open");
     faBars.classList.toggle("navbar-bars__icons__hide");
     faXmark.classList.toggle("navbar-bars__icons__hide");
+    navbarIconLogin.classList.toggle("hide");
   });
