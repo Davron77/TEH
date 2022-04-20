66be72e22de5b4f6bdf649d780380ef758844365
Davron77 <davronbekq467@gmail.com>
2022-04-20 13:44:31 +0500

    Header account add

--- javascript/script.js
+++ javascript/script.js
@@ -4,6 +4,11 @@ const faXmark = document.querySelector(".fa-xmark");
 const HeaderFooter = document.querySelector("#HeaderFooter");
 const navbarIconLogin = document.querySelector(".navbar-icon__login");
 const navbarDropdown = document.querySelector(".navbar-dropdown");
+const cardRegister = document.querySelector("#card__register");
+const cardLogin = document.querySelector("#card__login");
+const cardAccount = document.querySelector("#card__account");
+const bgDark = document.querySelector("#bg-dark");
+const user = document.getElementById("user");
 
 const navbarToggler = document
   .getElementById("navbar-toggler")
@@ -15,3 +20,11 @@ const navbarToggler = document
     faXmark.classList.toggle("navbar-bars__icons__hide");
     navbarIconLogin.classList.toggle("hide");
   });
+
+user.addEventListener("click", function () {
+  cardRegister.classList.toggle("Open__displayBlock");
+  //cardLogin.classList.toggle("Open__displayBlock");
+  //cardAccount.classList.toggle("Open__displayBlock");
+  bgDark.classList.toggle("Open__displayBlock");
+  user.classList.toggle("header-user-show");
+});
