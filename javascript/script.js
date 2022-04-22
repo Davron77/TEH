7644d6f3d921dfd812a633e1a1d1e502d7c4a34a
Davron77 <davronbekq467@gmail.com>
2022-04-22 16:59:14 +0500

    Add products page

--- javascript/script.js
+++ javascript/script.js
@@ -65,14 +65,14 @@ var swiper = new Swiper(".mySwiper", {
       slidesPerView: 3,
     },
     1200: {
-      slidesPerView: 4,
+      slidesPerView: 6,
     },
   },
 });
 
-// product-card
+// index-page-product-card
 
-var swiper = new Swiper(".product", {
+var swiper = new Swiper(".index-page-product", {
   slidesPerView: 4,
   spaceBetween: 30,
   slidesPerGroup: 1,
@@ -97,6 +97,66 @@ var swiper = new Swiper(".product", {
     },
     1200: {
       slidesPerView: 4,
+    },
+  },
+});
+
+// product-card
+
+var swiper = new Swiper(".product-Swiper", {
+  slidesPerView: 6,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1.5,
+    },
+    400: {
+      slidesPerView: 2,
+    },
+    767: {
+      slidesPerView: 3,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4.5,
+    },
+  },
+});
+
+var swiper = new Swiper(".product", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1.1,
+    },
+    500: {
+      slidesPerView: 1.5,
+    },
+    767: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 2.5,
+    },
+    1200: {
+      slidesPerView: 3,
     },
   },
 });
